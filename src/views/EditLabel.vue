51acd335be54ee1caf8c063cc289748d858831c7
xinouf <852618183@qq.com>
2021-08-27 14:02:52 +0800

    编辑表情

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,19 +1,19 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"/>
+      <Icon class="leftIcon" name="left" @click.native="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
     <FormItem
         :value="tag.name"
-        ssh-keygen -t rsa -C "your_email@example.com"@update:value="updateTag"
+        @update:value="update"
         field-name="标签名"
         placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
 
   </Layout>
@@ -46,8 +46,18 @@ export default class EditLabel extends Vue {
     }
 
   }
-  updateTag(event:InputEvent){
-
+  update(name:string){
+    if(this.tag){
+      tagListModel.update(this.tag.id,name)
+    }
+  }
+  remove(){
+    if(this.tag){
+      tagListModel.remove(this.tag.id)
+    }
+  }
+  goBack(){
+    this.$router.back()
   }
 }
 </script>
